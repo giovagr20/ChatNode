592304f8eb9f4ad38bce4a5661effd3df3701c0c
Giovanni Gómez Restrepo <giovagr20@users.noreply.github.com>
2019-10-28 18:02:25 -0500

    Ok

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -6,6 +6,31 @@ $(function(){
     var $messageBox = $('#message');
     var $chat = $('#chat');
 
+    //Obtaning DOM from nicknames
+    const $nickForm = $('#nickForms');
+    const $nickError = $('#nickError');
+    const $nickname = $('#nickname');
+
+    const $users = $('#usernames');
+
+    $nickForm.submit(e =>{
+        e.preventDefault();
+        socket.emit('new user', $nickname.val(), data =>{
+            if(data){
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+            }else{
+                $nickError.html(`
+                <div class="alert alert-danger">
+                That username already exists
+                </div>
+                `);
+            }
+            $nickname.val('');
+        });
+
+    });
+
     //Events
     $messageForm.submit( e => {
         e.preventDefault();
--- src/sockets.js
+++ src/sockets.js
@@ -1,9 +1,28 @@
 module.exports = function(io){
+
+    let nicknames = [];
+
     io.on('connection', socket => {
     console.log('new user connected');
+
+    socket.on('new user', (data, cb)=>{
+        console.log(data);
+        if(nicknames.indexOf(data) != -1){
+            cb(false);
+        }
+        else{
+            cb(true);
+            socket.nickname = data;
+            nicknames.push(socket.nickname);
+        }
+
+    });
+
     socket.on('send message', (data)=>{
         //io todos los usuarios conectados
         io.sockets.emit('new message', data);
     });
+
+
 });
 }
