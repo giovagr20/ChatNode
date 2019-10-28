74566f8d923aada9d1d40439368233c0f66a207d
Giovanni Gómez Restrepo <giovagr20@users.noreply.github.com>
2019-10-28 13:10:40 -0500

    Chat OK -- sockets

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,21 @@
-io();
+$(function(){
+    const socket = io();
+
+    //Get parts DOM from UI
+    var $messageForm = $('#message-form');
+    var $messageBox = $('#message');
+    var $chat = $('#chat');
+
+    //Events
+    $messageForm.submit( e => {
+        e.preventDefault();
+       socket.emit('send message', $messageBox.val());
+       $messageBox.val('');
+    });
+
+    socket.on('new message', function(data){
+        $chat.append(data + '<br>');
+
+    });
+
+})
--- src/sockets.js
+++ src/sockets.js
@@ -1,5 +1,9 @@
 module.exports = function(io){
     io.on('connection', socket => {
     console.log('new user connected');
-})
+    socket.on('send message', (data)=>{
+        //io todos los usuarios conectados
+        io.sockets.emit('new message', data);
+    });
+});
 }
