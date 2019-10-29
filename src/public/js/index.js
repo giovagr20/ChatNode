2a2a01d66b4ebca3446f516a1ac238c8afd838b1
Giovanni Gómez Restrepo <giovagr20@users.noreply.github.com>
2019-10-28 22:26:56 -0500

    OK

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -12,6 +12,9 @@ $(function(){
     const $nickname = $('#nickname');
 
     const $users = $('#usernames');
+    const $catchnick = $('#catchnick');
+    
+
 
     $nickForm.submit(e =>{
         e.preventDefault();
@@ -26,6 +29,7 @@ $(function(){
             if(data){
                 $('#nickWrap').hide();
                 $('#contentWrap').show();
+                $('#catchnick').html(`<b>@${$nickname.val()} </b>`)
             }else{
                 $nickError.html(`
                 <div class="alert alert-danger">
@@ -44,20 +48,36 @@ $(function(){
     //Events
     $messageForm.submit( e => {
         e.preventDefault();
-       socket.emit('send message', $messageBox.val());
+       socket.emit('send message', $messageBox.val(), data =>{
+          $chat.append(`<p class="error">${data}</p>`);
+       });
        $messageBox.val('');
     });
 
     socket.on('new message', function(data){
-        $chat.append('<i class="fas fa-portrait"></i><b>' + data.nick + '</b>: ' + data.msg + '</br><hr>');
+        $chat.append('<i class="fas fa-at"></i><b>' + data.nick + '</b>: ' + data.msg + '</br><hr>');
+       
     });
 
+    socket.on('exit', data=>{
+       $chat.append(`<p> <i class="fas fa-sign-out-alt"></i> ${data} logout! <p><hr>`);
+      
+    })
+
+    socket.on('username', data=>{
+        $chat.append(`<p> <i class="fas fa-sign-in-alt"></i> ${data} has joined! <p><hr>`);
+    });
     socket.on('usernames', data=>{
         let html = '';
         for (let i=0; i< data.length; i++){
-            html += `<p><i class="fas fa-user"></i>${data[i]}</p><hr>`
+            html += `<p><i class="fas fa-user"></i> ${data[i]}</p><hr>`
         }
         $users.html(html);
-    })
+    });
+
+    socket.on('whisper', data=>{
+        $chat.append(`<p class="whisper"><i class="fas fa-at"></i><b>${data.nick}:</b> ${data.msg}</p><hr>`);
+
+    });
 
 })
--- src/sockets.js
+++ src/sockets.js
@@ -1,40 +1,72 @@
 module.exports = function(io){
 
-    let nicknames = [];
+    let nicknames = {};
 
     io.on('connection', socket => {
     console.log('new user connected');
 
+    
     socket.on('new user', (data, cb)=>{
-        console.log(data);
-        if(nicknames.indexOf(data) != -1){
+        //console.log(data);
+        if(data in nicknames){
             cb(false);
         }
         else{
             cb(true);
+            
             socket.nickname = data;
-            nicknames.push(socket.nickname);
+            nicknames[socket.nickname] = socket;
             updateNicknames();
+            
         }
 
     });
 
-    socket.on('send message', (data)=>{
+    socket.on('send message', (data, cb)=>{
+        
+        var msg = data.trim();
+
+        if(msg.substr(0,1)=== '@'){
+            msg = msg.substr(1);
+            const index = msg.indexOf(' ');
+            if(index !== -1){
+               var name = msg.substring(0, index);
+               var msg= msg.substring(index + 1);
+               if(name in nicknames){
+                   nicknames[name].emit('whisper', {
+                       msg,
+                       nick: socket.nickname
+                   });
+               } else{
+                   cb('Error! Please enter a valid user');
+               }
+            }else{
+                cb('Error! Please enter your message');
+            }
+        }else{
         //io todos los usuarios conectados
         io.sockets.emit('new message', {
             msg: data,
             nick: socket.nickname
         });
+    }
     });
 
     socket.on('disconnect', data=>{
         if(!socket.nickname) return;
-        nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-        updateNicknames();
+        delete nicknames[socket.nickname];
+        logoutUser();
     });
 
-    function updateNicknames(){
-        io.sockets.emit('usernames', nicknames);
+ 
+    function logoutUser(){
+        io.sockets.emit('exit', socket.nickname);
     }
+
+    function updateNicknames(){
+        io.sockets.emit('usernames', Object.keys(nicknames));
+        io.sockets.emit('username', socket.nickname);
+    }      
+    
 });
 }
